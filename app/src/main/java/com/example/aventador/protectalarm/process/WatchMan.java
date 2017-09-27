4868b485abbfed98c57f88684ae914057cef0b8e
UPPER\Aventador <giangrasso.remy@gmail.com>
2017-09-27 22:08:44 +0200

    New processing for brute force attack detection
    Signed-off-by: UPPER\Aventador <giangrasso.remy@gmail.com>

--- app/src/main/java/com/example/aventador/protectalarm/process/WatchMan.java
+++ app/src/main/java/com/example/aventador/protectalarm/process/WatchMan.java
@@ -2,6 +2,7 @@ package com.example.aventador.protectalarm.process;
 
 import android.app.Activity;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.comthings.gollum.api.gollumandroidlib.GollumDongle;
 import com.comthings.gollum.api.gollumandroidlib.callback.GollumCallbackGetBoolean;
@@ -10,6 +11,7 @@ import com.example.aventador.protectalarm.tools.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -19,21 +21,23 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class WatchMan {
 
     private static int DELAY_PACKETS_RSSI_MS = 100;
+    private static int FREQUENCY_BY_CHANNELS = 25000;
+    private static int NB_CHANNELS = 5; // 5 by default.
+    private static int NB_SCAN_BY_SEQUENCE = 4; // interval -> [1 , MAX_INT] one sequence -> 20 scan for each sequence.
 
     /*
     For Guardian mode
      */
-    private static int GUARDIAN_NB_SCAN_BY_SEQUENCE = 20; // interval -> [1 , MAX_INT] one sequence -> 20 scan for each sequence.
-    private static int PEAK_TOLERANCE = 10; // interval -> [1 , GUARDIAN_NB_SCAN_BY_SEQUENCE] in one sequence, if number of anormal peack is better than PEAK_TOLERANCE
+    private static int GUARDIAN_NB_SEQUENCES = 40;
+    private static int PEAK_TOLERANCE = 20; // interval -> [1 , GUARDIAN_NB_SCAN_BY_SEQUENCE] in one sequence, if number of anormal peack is better than PEAK_TOLERANCE
                                             // it's a brute force attack.
+    private static double MARGIN_ERROR = 0.1;
                                             // TODO: PEAK_TOLERANCE should be represented by a seek bar.
-    private static int NBCHANNELS = 5; // 5 by default.
 
     /*
     For discovery mode
      */
-    private static int DISCOVERY_NB_SEQUENCES = 20; // 20 sequences of scan will be executed
-    private static int DISCOVERY_NB_SCAN_BY_SEQUENCE = 4; // for each sequence executed, 4 scans will be executed.
+    private static int DISCOVERY_NB_SEQUENCES = 8; // 20 sequences of scan will be executed
 
     private static WatchMan instance;
     private AtomicBoolean guardianIsRunning;
@@ -88,7 +92,7 @@ public class WatchMan {
      * @param cbDiscoveryDone
      * @return
      */
-    public boolean startDiscovery(final Activity activity, int frequency, final GollumCallbackGetInteger cbDiscoveryDone) {
+    public boolean startDiscovery(final Activity activity, final int frequency, final GollumCallbackGetInteger cbDiscoveryDone) {
         Logger.d(TAG, "guardianIsRunning.get():" + guardianIsRunning.get() + " discoverIsRunning ?: " + discoverIsRunning.get() + " specan run:" + specanIsRunning);
         if (guardianIsRunning.get() || !discoverIsRunning.compareAndSet(false, true) || specanIsRunning) {
             return false;
@@ -97,7 +101,7 @@ public class WatchMan {
         startSpecan(activity, frequency, new GollumCallbackGetBoolean() {
             @Override
             public void done(boolean b) {
-                discoverThread = new DiscoverThread(activity, cbDiscoveryDone);
+                discoverThread = new DiscoverThread(activity, frequency, cbDiscoveryDone);
                 discoverThread.start();
             }
         });
@@ -111,7 +115,7 @@ public class WatchMan {
      * @param cbDone
      */
     private void startSpecan(final Activity activity, int frequency, final GollumCallbackGetBoolean cbDone) {
-        GollumDongle.getInstance(activity).rfSpecanStart(0, frequency, 25000, NBCHANNELS, DELAY_PACKETS_RSSI_MS, new GollumCallbackGetInteger() {
+        GollumDongle.getInstance(activity).rfSpecanStart(0, frequency, FREQUENCY_BY_CHANNELS, NB_CHANNELS, DELAY_PACKETS_RSSI_MS, new GollumCallbackGetInteger() {
             @Override
             public void done(int i) {
                 specanIsRunning = true;
@@ -186,10 +190,12 @@ public class WatchMan {
         private Activity activity;
         private ArrayList<Integer> allMeans;
         private AtomicBoolean run;
+        private int frequencyTarget;
         // a bucket represent the mean of a sequence of scans. In this case, for each bucket, 4 scan will be executed (scan is a getRssi())
 
-        public DiscoverThread(Activity activity, GollumCallbackGetInteger cbDone) {
+        public DiscoverThread(Activity activity, int frequencyTarget, GollumCallbackGetInteger cbDone) {
             this.activity = activity;
+            this.frequencyTarget = frequencyTarget;
             this.cbDone = cbDone;
             this.allMeans = new ArrayList<>();
             this.run = new AtomicBoolean(false);
@@ -206,7 +212,7 @@ public class WatchMan {
             for (int i = DISCOVERY_NB_SEQUENCES; i > 0; i--) { // for each sequence
                 // a sequence represent the mean of all scan's result in the current sequence.
                 Logger.d(TAG, "new bucket: " + i);
-                for (int j = DISCOVERY_NB_SCAN_BY_SEQUENCE; j > 0; j--) { // for each scan.
+                for (int j = NB_SCAN_BY_SEQUENCE; j > 0; j--) { // for each scan.
                     // for each sequence, NB_SCAN_BY_SEQUENCE scans (getRssi) will be executed.
                     if (!this.run.get()) {
                         return;
@@ -215,13 +221,20 @@ public class WatchMan {
                     rssi_buffer = scan(); // scan
                     if (rssi_buffer != null) { // if null -> buffer not ready.
                         Logger.d(TAG, "rssi: " + Arrays.toString(rssi_buffer));
-                        for (int k = 0; k < NBCHANNELS; k++) {
+                        for (int k = 0; k < NB_CHANNELS; k++) {
                             sum += rssi_buffer[k];
                             nbElements++;
                         }
                     }
                 }
-                allMeans.add(sum / nbElements);
+                if (nbElements == 0) {
+                    Logger.d(TAG, "DiscoverThread : rssi_buffer always empty");
+                    allMeans.add(Integer.MIN_VALUE);
+                } else {
+                    allMeans.add(sum / nbElements);
+
+                }
+
                 sum = 0;
                 nbElements = 0;
             }
@@ -239,10 +252,10 @@ public class WatchMan {
          */
         @Nullable
         private byte[] scan() {
-            byte[] rssi_buffer = new byte[NBCHANNELS];
-            int check = GollumDongle.getInstance(activity).rfSpecanGetRssi(rssi_buffer, NBCHANNELS);
+            byte[] rssi_buffer = new byte[NB_CHANNELS];
+            int check = GollumDongle.getInstance(activity).rfSpecanGetRssi(rssi_buffer, NB_CHANNELS);
             Logger.d(TAG, "check: " + check);
-            if (check != NBCHANNELS) {
+            if (check != NB_CHANNELS) {
                 return null;
             }
             return rssi_buffer;
@@ -268,33 +281,48 @@ public class WatchMan {
         @Override
         public void run() {
             run = new AtomicBoolean(true);
-            int scanIteration = 0;
             int peakDetected = 0;
+            int sum = 0;
+            int nbElements = 0;
+            int means;
             byte[] rssi_buffer;
-            while (run.get()) {
-                while (scanIteration < WatchMan.GUARDIAN_NB_SCAN_BY_SEQUENCE) {
-                    if (!run.get()) {
-                        return;
-                    }
 
-                    rssi_buffer = scan();
-                    if (rssi_buffer != null) {
-                        Logger.d(TAG, "rssi: " + Arrays.toString(rssi_buffer));
-                        for (int i = 0 ; i < NBCHANNELS ; i++) {
-                            if (rssi_buffer[i] > (dbTolerance + (-0.10 * dbTolerance))) {
-                                peakDetected++;
-                                break;
+            while (run.get()) {
+                for (int i = GUARDIAN_NB_SEQUENCES; i > 0; i--) { // for each sequence
+                    for (int j = NB_SCAN_BY_SEQUENCE; j > 0; j--) { // for each scan.
+                        if (!run.get()) {
+                            return;
+                        }
+
+                        rssi_buffer = scan();
+                        if (rssi_buffer != null) {
+                            Logger.d(TAG, "rssi: " + Arrays.toString(rssi_buffer));
+                            for (int k = 0 ; k < NB_CHANNELS ; k++) {
+                                sum += rssi_buffer[k];
+                                nbElements++;
                             }
                         }
                     }
+                    if (nbElements == 0) {
+                        Log.e(TAG, "rssi_buffer always empty !");
+                        means = Integer.MIN_VALUE;
+                    } else {
+                        means = sum / nbElements;
+                    }
+                    Log.d(TAG, "means is: " + means);
+                    nbElements = 0;
+                    sum = 0;
+                    if (means > (dbTolerance + Math.abs(MARGIN_ERROR * dbTolerance))) {
+                        peakDetected++;
+                        Logger.d(TAG, "peak detected, peakDetected: " + peakDetected);
+                    }
 
-                    scanIteration++;
                 }
+
                 if (peakDetected > WatchMan.PEAK_TOLERANCE) {
                     Logger.d(TAG, "attack detected");
                     this.cbAttackDetected.done(true);
                 }
-                scanIteration = 0;
                 peakDetected = 0;
             }
         }
@@ -305,9 +333,9 @@ public class WatchMan {
          */
         @Nullable
         private byte[] scan() {
-            byte[] rssi_buffer = new byte[NBCHANNELS];
-            int check = GollumDongle.getInstance(activity).rfSpecanGetRssi(rssi_buffer, NBCHANNELS);
-            if (check != NBCHANNELS) {
+            byte[] rssi_buffer = new byte[NB_CHANNELS];
+            int check = GollumDongle.getInstance(activity).rfSpecanGetRssi(rssi_buffer, NB_CHANNELS);
+            if (check != NB_CHANNELS) {
                 return null;
             }
             return rssi_buffer;
