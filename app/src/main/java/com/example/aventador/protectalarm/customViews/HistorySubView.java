c698815a18e34e3219f285196f233bf5800b5b95
UPPER\Aventador <giangrasso.remy@gmail.com>
2017-10-19 12:29:23 +0200

    add Clear function into HistorySubView
    Signed-off-by: UPPER\Aventador <giangrasso.remy@gmail.com>

--- app/src/main/java/com/example/aventador/protectalarm/customViews/HistorySubView.java
+++ app/src/main/java/com/example/aventador/protectalarm/customViews/HistorySubView.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -24,6 +25,7 @@ public class HistorySubView extends GuardianSubView {
 
     private ListView listViewLogs;
     private View layout;
+    private Button clearButton;
     private Context context;
 
     public HistorySubView(Context context, String title, int layoutResId) {
@@ -32,6 +34,7 @@ public class HistorySubView extends GuardianSubView {
     }
 
     public void addLog(HistoryLog log) {
+        clearButton.setVisibility(View.VISIBLE);
         LogAdapter logAdapter = (LogAdapter) listViewLogs.getAdapter();
         logAdapter.add(log);
         logAdapter.notifyDataSetChanged();
@@ -42,12 +45,19 @@ public class HistorySubView extends GuardianSubView {
         layout = inflater.inflate(getLayoutResId(), viewGroup, false);
         listViewLogs = (ListView) layout.findViewById(R.id.history_listview);
         listViewLogs.setAdapter(new LogAdapter(context, 0, new ArrayList<HistoryLog>()));
+        clearButton = (Button) layout.findViewById(R.id.clear_button_history);
+        clearButton.setOnClickListener(this);
+        clearButton.setVisibility(View.GONE);
         return layout;
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view == clearButton) {
+            LogAdapter logAdapter = (LogAdapter) listViewLogs.getAdapter();
+            logAdapter.clear();
+            logAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
